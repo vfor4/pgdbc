d57a2bfc983e123685d3eff74dd5abeb3c60ed65
vfor4 <trandangvu2708@gmail.com>
2024-04-16 20:54:12 +0700

    brzzzzzzzzzzz

--- cmd/logservice/main.go
+++ cmd/logservice/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"context"
+	"fmt"
 	stlog "log"
 	"main/log"
+	"main/registry"
 	"main/service"
 )
 
 func main() {
 	log.Run("./app.log")
 	host, port := "localhost", "4000"
+	serviceAddress := fmt.Sprintf("http://%v:%v", host, port)
 
-	ctx, err := service.Start(context.Background(), "Log Service", host, port, log.RegisterHandlers)
+	var r registry.Registration
+	r.ServiceName = registry.LogService
+	r.ServiceUrl = serviceAddress
+
+	ctx, err := service.Start(context.Background(), host, port, r, log.RegisterHandlers)
 	if err != nil {
 		stlog.Fatal(err)
 	}
