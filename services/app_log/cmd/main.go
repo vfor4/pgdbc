ce61d8cc0aee871e96f521fbcac52263402c84a4
vfor4 <trandangvu2708@gmail.com>
2024-05-06 22:18:56 +0700

    everything must be made as simple as possible. But not simpler

--- services/app_log/cmd/main.go
+++ services/app_log/cmd/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	stlog "log"
+	// stdlog "log"
 	applog "main/services/app_log"
+	"main/services/registry"
+	"main/services/runner"
 )
 
 func main() {
 	applog.Run("logs")
 
 	host, port := "localhost", "4000"
-	serviceAddress := fmt.Sprintf("http://%v:%v", host, port)
+
+	// serviceAddress := fmt.Sprintf("http://%v:%v", host, port)
+
+	var r registry.Registration
+	r.ServiceName = registry.LogService
+
+	ctx := runner.Start(context.Background(), host, port, r, applog.RegisterHandler)
+
+	<-ctx.Done()
+	fmt.Println("Shutting down LogService")
 }
